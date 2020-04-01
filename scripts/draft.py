a137cbc42c4047b635ab358a2c80ce5e5181be1d
ngshya <ngshya@gmail.com>
2020-04-02 01:04:13 +0200

    testing code

--- scripts/draft.py
+++ scripts/draft.py
@@ -1,26 +1,17 @@
-import io
-import pandas as pd
-import pickle
 from sdaab.disk.storage_disk import StorageDisk
-s = StorageDisk("/home/ngshya/projects/sdaab/tmp/")
+from sdaab.s3boto.storage_s3_boto import StorageS3boto
+from sdaab.utils.get_config import dict_config
+
+
+disk_root_path = dict_config["DISK"]["ROOT_PATH"]
+
+
+s = StorageDisk(root_path=disk_root_path)
+
 s.ls()
+s.mkdir("folder1")
+s.ls()
+my_variable = "Hello World!"
 
-dtf_tmp = pd.DataFrame({"A": [1,2,3], "B": [4,5,6]})
-s.upload_from_memory(dtf_tmp, "dtf_tmp")
+s.upload_from_memory(my_variable, "my_variable")
 
-
-
-'''
-s.upload_from_memory(dtf_tmp, file="dtf_tmp")
-dtf_tmp_2 = s.download_to_memory(file="dtf_tmp")
-
-
-
-f = io.StringIO()
-dtf_tmp.to_csv(f)
-s.upload(f, "nome_che_voglio")
-
-
-f = s.download("nome_che_voglio")
-s.load(f)
-'''
